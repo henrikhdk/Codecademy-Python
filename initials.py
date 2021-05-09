c4db31547afbc66896e46d03a8a808daf13a2231
Henrik Doark <henrikdoark@gmail.com>
2021-05-09 12:18:18 -0300

    initial commit

---
+++ initials.py
@@ -0,0 +1,14 @@
+# My First Name is Henrik, my last name is Doark.
+# This is my firt time playing with my initials on ASCII Art style.
+
+HD = """
+H   H  DDDD
+H   H  D    D
+H   H  D    D
+HHHHH  D    D
+H   H  D    D
+H   H  D    D
+H   H  DDDD
+"""
+
+print(HD)
