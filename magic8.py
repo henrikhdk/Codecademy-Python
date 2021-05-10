6163786ae282e9fbe801be1da950c590c16912ae
Henrik Doark <henrikdoark@gmail.com>
2021-05-10 13:26:06 -0300

    Update the code to the Challenge of implemented the full 20 possible answers from the standard Magic 8 Ball

--- magic8.py
+++ magic8.py
@@ -1,3 +1,4 @@
+# Importa the random Lib to generate a magic 8 ball number
 import random
 
 name = "Henrik"
@@ -9,24 +10,52 @@ answer = ""
 random_number = random.randint(1, 20)
 
 #print(random_number)
+
+# 10 affirmative answers
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "It is Certain."
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = "It is decidedly so."
 elif random_number == 3:
   answer = "Without a doubt."
 elif random_number == 4:
-  answer = "Reply hazy, try again."
+  answer = "Yes definitely."
 elif random_number == 5:
-  answer = "Ask again later."
+  answer = "You may rely on it."
 elif random_number == 6:
-  answer = "Better not tell you now."
+  answer = "As I see it, yes."
 elif random_number == 7:
-  answer = "My sources say no."
+  answer = "Most likely."
 elif random_number == 8:
   answer = "Outlook not so good."
 elif random_number == 9:
-  answer = "Very doubtful."
+    answer = "Yes."
+elif random_number == 10:
+    answer = "Signs point to yes."
+
+# 5 non-committal answers
+elif random_number == 11:
+    answer = "Reply hazy, try again."
+elif random_number == 12:
+    answer = "Ask again later."
+elif random_number == 13:
+    answer = "Better not tell you now."
+elif random_number == 14:
+    answer = "Cannot predict now."
+elif random_number == 15:
+    answer = "Concentrate and ask again."
+
+# 5 negative answers
+elif random_number == 16:
+    answer = "Don't count on it."
+elif random_number == 17:
+    answer = "My reply is no."
+elif random_number == 18:
+    answer = "My sources say no."
+elif random_number == 19:
+    answer = "Outlook not so good."
+elif random_number == 20:
+    answer = "Very doubtful."
 else:
   answer = "Error"
 
